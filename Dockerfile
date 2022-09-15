ace7ef704580e2989efae2a1876bb54759ca9c93
Philip <philipandersson27@gmail.com>
2022-09-15 20:40:29 +0200

    Add first version of echo REST microservice and add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:latest
+
+RUN mkdir /build
+WORKDIR /build
+
+RUN export GO111MODULE=on
+RUN go get github.com/CSCphilip/echo-REST-microservice/main
+RUN cd /build && git clone https://github.com/CSCphilip/echo-REST-microservice.git
+
+RUN cd /build/echo-REST-microservice/main && go build
+
+EXPOSE 8080
+
+ENTRYPOINT [ "/build/echo-REST-microservice/main/main" ]
---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type echo struct {
+	Message string `json:"message"`
+}
+
+func postEcho(c *gin.Context) {
+	var echoMessage echo
+	
+	if err := c.BindJSON(&echoMessage); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"err":"Something went wrong.", "code":400})
+		return
+    }
+
+	c.JSON(http.StatusCreated, echoMessage)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/echo", postEcho)
+
+	router.Run("localhost:8080")
+}
